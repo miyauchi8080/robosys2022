a505a056f88f8dc2eb122512b1e62dc6abfdfca6
kojimiyauchi <s21c1121uz@s.chibakoudai.jp>
2022-10-27 12:24:39 +0900

    test command

--- test.bash
+++ test.bash
@@ -1,6 +1,16 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: Koji Miyauchi
+# SPDX-License-Identifier: BSD-3-Clause
+ng (){
+	echo NG at Line $1
+	res=1
+}
+
+res=0
 
 out=$(seq 5 | ./plus)
 
-[ "${out}" = 15 ]
+[ "${out}" = 14 ] || ng ${LINENO}
+[ "$res" = 0 ] && echo OK
+exit $res
 
